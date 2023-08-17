5d14a3afd8e359f5561ee72de422cbf64d3e4c44
ajmal-j <141622726+ajmal-j@users.noreply.github.com>
2023-08-17 17:11:01 +0530

    amazon.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -28,21 +28,6 @@ products.forEach((product) => {
         <span style="font-size:25px;">&#x20B9;. </span>${formatCurrency(product.pricePaisa)}
       </div>
 
-      <div class="product-quantity-container">
-        <select>
-          <option selected value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
-          <option value="6">6</option>
-          <option value="7">7</option>
-          <option value="8">8</option>
-          <option value="9">9</option>
-          <option value="10">10</option>
-        </select>
-      </div>
-
       <div class="product-spacer"></div>
 
       <button class="add-to-cart-button button-primary js-add-to-cart"
